3990bcd29bd1486d1b6b5a4bb50583ca8c61ce42
Jay Khatri <jaykhatrimail@gmail.com>
2025-02-19 13:48:04 -0600

    add flask instro

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from functools import wraps
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from datetime import datetime, timedelta
@@ -25,32 +26,22 @@ logger.info("Starting the application")
 
 app = Flask(__name__)
 
-def request_hook(span: Span, request: Any):
-    if span and span.is_recording():
-        user_id = request.headers.get("user.id", "unknown")
-        span.set_attribute("user.id", user_id)
+# Your custom decorator
+def around_wrapper(fn):
+    @wraps(fn)
+    def wrapped(*args, **kwargs):
+        logger.info("Before view function")
+        result = fn(*args, **kwargs)
+        logger.info("After view function")
+        return result
+    return wrapped
 
-def response_hook(span: Span, status: str, response_headers: List):
-    if span and span.is_recording():
-        span.set_attribute("custom_user_attribute_from_response_hook", "some-value")
-        for header, value in response_headers:
-            span.set_attribute(f"http.response.header.{header.lower().replace('-', '_')}", value)
-    
-FlaskInstrumentor().instrument_app(app, excluded_urls="", request_hook=request_hook, response_hook=response_hook)
+FlaskInstrumentor().instrument_app(app, excluded_urls="")
 
 RequestsInstrumentor().instrument()
 
-@app.after_request
-def after_request(response):
-    with tracer.start_as_current_span(f"{request.method} {request.path}", context=request.traceparent):
-        counter.add(1)
-        end_time = datetime.now()
-        time_delta: timedelta = end_time - request.start_time
-        histogram.record(time_delta.total_seconds())
-        gauge.set(time_delta.total_seconds())
-    return response
-
 @app.route("/", methods=["GET"])
+@around_wrapper
 def health():
     logger.info("Endpoint called")
     response = requests.get("http://httpbin.org/headers")
